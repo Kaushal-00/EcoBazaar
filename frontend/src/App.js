7cf3296f94f6a7522d16e26ec05888aeb1aed0fd
Kaushal-00 <kaushalkathiriya2005@gmail.com>
2025-08-28 01:16:47 +0530

    chore: integrate Filter component into App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,20 @@ import Filters from "./components/Filter/Filter";
 function App() {
   return (
     <>
-      {/* Navbar on top */}
       <Navbar />
 
-      {/* Main Layout */}
       <div className="container mt-4">
+        
         <div className="row">
-          {/* Filter Sidebar */}
+          
           <div className="col-md-3">
             <Filters />
           </div>
 
-          {/* Product Section */}
-          <div className="col-md-9">
-            <h1>Eco-Friendly Products</h1>
-          </div>
+          <div className="col-md-9"> </div>
+
         </div>
+
       </div>
     </>
   );
