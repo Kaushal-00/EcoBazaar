3859f61da5333ec57b84501f9f9b3ca21be98637
Kaushal-00 <kaushalkathiriya2005@gmail.com>
2025-08-29 01:28:15 +0530

    Added Customer Dashboard page by integrating Navbar, Filters, ProductGrid, and Footer components; cleaned up App.js to render Customer Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Filters from "./components/Filter/Filter";
-import ProductGrid from "./components/ProductGrid/ProductGrid";
-import Footer from "./components/Footer/Footer";
+import Customer_Dashboard from "./pages/Customer_Dashboard/Customer_Dashboard";
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Navbar />
-      <div className="container mt-4">
-        <div className="row position-relative g-4">
-          <div className="col-lg-3 col-md-4">
-            <Filters />
-          </div>
-          <div className="col-lg-9 col-md-8">
-            <ProductGrid />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
+    <>
+          <Customer_Dashboard />
+    </>
   );
 }
 
---
+++ frontend/src/pages/Customer_Dashboard/Customer_Dashboard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Filters from "../../components/Filter/Filter";
+import ProductGrid from "../../components/ProductGrid/ProductGrid";
+import Footer from "../../components/Footer/Footer";
+
+export default function Customer_Dashboard() {
+  return (
+    <>
+        <div className="app-wrapper">
+            <Navbar />
+            <div className="container mt-4">
+                <div className="row position-relative g-4">
+                <div className="col-lg-3 col-md-4">
+                    <Filters />
+                </div>
+                <div className="col-lg-9 col-md-8">
+                    <ProductGrid />
+                </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    </>
+  )
+}
